339ef4e25374a59a21c775bee467ad535a28c1a6
Jose Benitez <pepebenitez12@gmail.com>
2020-12-24 12:35:44 -0500

    Learned about event handling with v-on

--- app.js
+++ app.js
@@ -4,11 +4,25 @@ new Vue({
         name: 'Pepe',
         website: 'http://www.google.com',
         websiteTag: '<a href="www.youtube.com">Another Website</a>',
+        age: 28,
+        x: 0,
+        y: 0,
     },
     methods: {
 		greet: function(time){
 			return 'Good ' + time + ' ' + this.name;
-		}
+        },
+        add: function(){
+            this.age++;
+        },
+        subtract: function(){
+            this.age--;
+        },
+        updateXY: function(event){
+            console.log(event);
+            this.x = event.offsetX;
+            this.y = event.offsetY;
+        },
 	}
 
 });
